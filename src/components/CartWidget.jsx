e227ebe443b5f406bd536ddfe7fa0b8330895aeb
JorgitoHD <martinoli_matias99@hotmail.es>
2023-07-07 20:50:17 -0300

    Update

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+
+
+
+
+export const CartWidget = () =>{
+    return (
+        <i className="bi bi-cart2"></i>
+    )
+}
+export default CartWidget
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import logo from '../logo.png'
+import CartWidget from './CartWidget'
+const logocomponent= logo
+const AppNavbar = () => {
+  return (
+    <Navbar bg="success" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          src={logocomponent}
+          width="35"
+          height="40"
+          className="d-inline-block align-top"
+          alt="Logo"
+        />
+      </Navbar.Brand>
+      <medium>Welcome to The end</medium>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#shop">Shop</Nav.Link>
+          <NavDropdown title="Categories" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#Action">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#Adventure">Adventure</NavDropdown.Item>
+            <NavDropdown.Item href="#Racing">Racing</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#BrowseAll">Browse All</NavDropdown.Item>
+            </NavDropdown>
+          <Nav.Link href="#comunity">Comunity</Nav.Link>
+          <Nav.Link href="#support">Support</Nav.Link>
+          <Nav.Link href="#shopcart">Shopcart<CartWidget /></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
